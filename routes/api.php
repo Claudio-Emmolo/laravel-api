4ed1164dcb023158364050ff5c20d03a4295b594
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-06 16:03:29 +0100

    Created API token

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/projects', [ApiProjectController::class, 'index'])->name('api.project.index');
-Route::get('/projects/{project}', [ApiProjectController::class, 'show'])->name('api.project.show');
+Route::middleware('auth:api')->get('/projects', [ApiProjectController::class, 'index'])->name('api.project.index');
+Route::middleware('auth:api')->get('/projects/{project}', [ApiProjectController::class, 'show'])->name('api.project.show');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
